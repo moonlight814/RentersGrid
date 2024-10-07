64bbf312e06abebaa5f2d008c6f100ebe12ba5a2
Francisco Diaz <frankydiaz1999@gmail.com>
2024-10-07 15:22:33 -0700

    Made changes to add mongo DB

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,23 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
+# Load environment variables from the .env file
+load_dotenv()
+
+# Retrieve the MongoDB URI from the environment variables
+mongo_uri = os.getenv("MONGO_URI")
+
+# Connect to MongoDB
+client = MongoClient(mongo_uri)
+db = client['rentersDB']  # Use or create the 'rentersDB' database
+
+# Flask Application Setup
+app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return "Hello, Flask and MongoDB!"
 
 
 if __name__=="__main__":
