da372a6e7a3508000d2e46a371e4b3c39ed7d3f7
ArkZani <60407722+ArkZani@users.noreply.github.com>
2024-10-16 11:24:23 -0700

    Redirect user when clicking on account icon

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -5,7 +5,9 @@ import Helvetica from '../fonts/Helvetica.ttf'; // Adjust path as necessary
 import DownArrow from '../Assets/downward.svg'; // Make sure the path to the arrow is correct
 import SubmitLandlordRate from '../Assets/submit landlord rate.svg';
 import Map from '../components/Map';
+
 import SideMenu from './SideMenu'; // Import the SideMenu component
+
 import { useNavigate } from 'react-router-dom';
 import './HomePage.css';  // Create a CSS file for styling if needed
 
@@ -17,23 +19,17 @@ import './HomePage.css';  // Create a CSS file for styling if needed
     navigate(`/search-results?searchBy=${searchType}&query=${searchInput}`);
   };*/
 
+
+
+
+// Function to refresh the page
+const refreshPage = () => {
+    window.location.reload();
+}
+
 function HomePage() {
     const [selectedOption, setSelectedOption] = useState('landlord'); // Default option
     const [dropdownOpen, setDropdownOpen] = useState(false); // To toggle the dropdown
-    const [isSignedIn, setIsSignedIn] = useState(true); //track if user is signed in
-    const navigate = useNavigate(); 
-
-    // Function to refresh the page
-    const refreshPage = () => {
-        window.location.reload();
-    };
-
-    //function to handle sign out
-    const handleSignOut = () => {
-        setIsSignedIn(false); //reset sign in state
-        //redirect to the homepage or sign-in page
-        navigate('/'); //route to no account homepage
-    };
 
     const handleDropdownToggle = () => {
         setDropdownOpen(!dropdownOpen);
@@ -54,7 +50,7 @@ function HomePage() {
 
     return (
         <div className="main-container">
-            <SideMenu onSignOut={handleSignOut} /> {/* Pass handleSignOut as a prop */}
+            <SideMenu />
 
             <header className="headerhp">
                 <div className="logohp-container">
@@ -74,11 +70,14 @@ function HomePage() {
                     />
 
                     {/* Right Image: Account Button */}
+                    <a href="/account">
                     <img
                         src={AccountButton}
                         alt="Account Button"
                         className="account-right"
+                        
                     />
+                    </a>
                 </div>
             </header>
 
